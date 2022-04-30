cfec09da3cf5a76cdb58557948bf5d0ef821d777
Patricio Ernesto Antonio Arena <patricio.e.arena@gmail.com>
2022-04-30 20:25:19 -0300

    refactor: se cambia la forma en la que estaaban desarrolladas las vistas usando las virtudes dadas por hbs.

--- src/app.js
+++ src/app.js
@@ -32,6 +32,8 @@
 var os = require("os");
 var hostname = os.hostname();
 var express = require('express');
+var path = require("path");
+
 var handlebars = require('express-handlebars');
 var bodyParser = require('body-parser')
 require('dotenv').config()
@@ -56,35 +58,160 @@ function setExpirationDates(number) {
     return { from: _from, to: _to }
 }
 
+function priceToFraction(price) {
+
+    var result = price.toLocaleString('es-ar', {
+        style: 'currency',
+        currency: 'ARS',
+        minimumFractionDigits: 2
+    });
+
+    return result;
+}
+
 const payments = []
+const db = [
+    {
+        id: 1234,
+        description: "Dispositivo móvil de Tienda e-commerce: Samsung Galaxy S9",
+        img: "/assets/images/samsung-galaxy-s9-xxl.jpg",
+        title: "Samsung Galaxy S9",
+        price: 549599,
+        unit: 1
+    },
+    {
+        id: 3456,
+        description: "Dispositivo móvil de Tienda e-commerce: LG G6",
+        img: "/assets/images/l6g6.jpg",
+        title: "LG G6",
+        price: 25000.50,
+        unit: 1
+    },
+    {
+        id: 6456,
+        description: "Dispositivo móvil de Tienda e-commerce: iPhone 8",
+        img: "/assets/images/u_10168742.jpg",
+        title: "iPhone 8",
+        price: 116000.99,
+        unit: 1
+    },
+    {
+        id: 4567,
+        description: "Dispositivo móvil de Tienda e-commerce: Motorola G5",
+        img: "/assets/images/motorola-moto-g5-plus-1.jpg",
+        title: "Motorola G5",
+        price: 38000.50,
+        unit: 1
+    },
+    {
+        id: 5678,
+        description: "Dispositivo móvil de Tienda e-commerce: Moto G4",
+        img: "/assets/images/motorola-moto-g4-3.jpg",
+        title: "Moto G4",
+        price: 24000.50,
+        unit: 1
+    },
+    {
+        id: 4599,
+        description: "Dispositivo móvil de Tienda e-commerce: Sony Xperia XZ2",
+        img: "/assets/images/003.jpg",
+        title: "Sony Xperia XZ2",
+        price: 56000,
+        unit: 1
+    },
+    {
+        id: 4599,
+        description: "Dispositivo móvil de Tienda e-commerce: Asus ROG Phone 5 ZS673KS",
+        img: "/assets/images/ZS673KS.png",
+        title: "Asus ROG Phone 5 ZS673KS",
+        price: 280000,
+        unit: 1
+    },
+    {
+        id: 4599,
+        description: "Dispositivo móvil de Tienda e-commerce: Xiaomi Redmi Note 11",
+        img: "/assets/images/20220423223349.jpg",
+        title: "Xiaomi Redmi Note 11",
+        price: 55999.99,
+        unit: 1
+    },
+
+];
 
 app.use(bodyParser.urlencoded({ extended: false }))
-app.set('view engine', 'hbs');
-app.engine('hbs', handlebars({
-    layoutsDir: __dirname + '/views/layouts',
-    extname: 'hbs'
+
+app.set('view engine', '.hbs');
+app.set("views", path.join(__dirname, "views"));
+app.engine('.hbs', handlebars({
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    extname: '.hbs'
 }));
 
 app.use(express.static('assets'));
 app.use('/assets', express.static(__dirname + '/assets'));
 
-app.get('/', function (req, res) {
-    res.render('home');
-});
-
 app.get('/success', function (req, res) {
     var query = req.query
-    res.json(query);
+    // res.json(query);
+    query = {
+        "collection_id": "21874753905",
+        "collection_status": "approved",
+        "payment_id": "21874753905",
+        "status": "approved",
+        "external_reference": "patricio.e.arena@gmail.com",
+        "payment_type": "credit_card",
+        "merchant_order_id": "4617967805",
+        "preference_id": "469485398-d30a67f4-eebd-40a3-aacd-1c7af487a116",
+        "site_id": "MLA",
+        "processing_mode": "aggregator",
+        "merchant_account_id": "null"
+    }
+
+    // console.log(item);
+    res.render('./success/success', query);
 });
 
 app.get('/failure', function (req, res) {
     var query = req.query
-    res.json(query);
+    // res.json(query);
+    query = {
+        "collection_id": "21874753905",
+        "collection_status": "failure",
+        "payment_id": "21874753905",
+        "status": "failure",
+        "external_reference": "patricio.e.arena@gmail.com",
+        "payment_type": "credit_card",
+        "merchant_order_id": "4617967805",
+        "preference_id": "469485398-d30a67f4-eebd-40a3-aacd-1c7af487a116",
+        "site_id": "MLA",
+        "processing_mode": "aggregator",
+        "merchant_account_id": "null"
+    }
+
+    // console.log(item);
+    res.render('./failure/failure', query);
 });
 
 app.get('/pending', function (req, res) {
     var query = req.query
-    res.json(query);
+    // res.json(query);
+    query = {
+        "collection_id": "21874753905",
+        "collection_status": "pending",
+        "payment_id": "21874753905",
+        "status": "pending",
+        "external_reference": "patricio.e.arena@gmail.com",
+        "payment_type": "ticket",
+        "merchant_order_id": "4617967805",
+        "preference_id": "469485398-d30a67f4-eebd-40a3-aacd-1c7af487a116",
+        "site_id": "MLA",
+        "processing_mode": "aggregator",
+        "merchant_account_id": "null"
+    }
+
+    // console.log(item);
+    res.render('./pending/pending', query);
 });
 
 app.get('/payments', function (req, res) {
@@ -136,30 +263,19 @@ app.post('/detail', function (req, res) {
     var failure_url = `${hostname}/failure`;
     var pending_url = `${hostname}/pending`;
 
-    var body = req.body;
-
-    var quantity = Number(req.body.unit)
-    var unitPrice = Number(req.body.price)
-
-    req.query.id = req.body.id
-    req.query.description = req.body.description
-    req.query.title = req.body.title
-    req.query.img = req.body.img
-    req.query.unit = req.body.unit
+    var item = {
+        id: req.body.id,
+        title: req.body.title,
+        description: req.body.description,
+        picture_url: hostname + req.body.img,
+        unit_price: Number(req.body.price),
+        quantity: Number(req.body.unit),
+        currency_id: "ARS",
+        category_id: 'phones',
+    }
 
     let preference = {
-        items: [
-            {
-                id: body.id,
-                title: body.title,
-                description: req.body.description,
-                picture_url: hostname + req.body.img,
-                unit_price: unitPrice,
-                quantity: quantity,
-                currency_id: "ARS",
-                category_id: 'phones',
-            }
-        ],
+        items: [item],
         payer: {
             name: "Lalo",
             surname: "Landa",
@@ -205,14 +321,34 @@ app.post('/detail', function (req, res) {
 
     mercadopago.preferences.create(preference)
         .then(function (response) {
-            req.query.id = response.body.id;
-            req.query.init_point = response.body.init_point;
-            req.query.price = unitPrice;
-            res.render('detail', req.query);
+
+            var aux = {
+                preference_id: response.body.id,
+                init_point: response.body.init_point,
+                img: req.body.img,
+                fraction: req.body.fraction
+            }
+
+            var renderObj = { ...item, ...aux }
+            // console.log(response);
+
+            // res.json(response)
+            res.render('./detail/detail', renderObj);
+
         }).catch(function (error) {
             console.log(error);
         });
 
+});
+
+app.get('/', function (req, res) {
+
+    let items = db.map(item => {
+        item.fraction = priceToFraction(item.price)
+        return item;
+    });
+
+    res.render('./home/home', { items });
 });
 
 app.listen(port)
